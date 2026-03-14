6c25bfa05ce0b418aca55b3190133279742507b4
huncholane <huncho@hygo.ai>
2026-03-14 13:56:16 -0700

    fix: og twitter share

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -1,8 +1,6 @@
 import { ImageResponse } from "@vercel/og";
 import { getShare } from "@/lib/cache";
 
-export const runtime = "edge";
-
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
   const id = searchParams.get("id");
